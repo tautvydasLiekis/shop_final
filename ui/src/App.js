9f2904d533c0a7360d41ccac1e7d79d7fd9f17cc
Crystal <tautvydas.liekis@gmail.com>
2021-01-12 22:08:18 +0200

    added architecture to ui

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router} from "react-router-dom";
+import Header from "./components/Header";
+import Content from "./components/Content/Content";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Header/>
+        <Content/>
+        <Footer/>
+      </Router>
   );
 }
 
