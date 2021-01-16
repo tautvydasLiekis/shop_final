4fc03866de8b407fd532ef6c5a5541dfb41c1b65
Crystal <tautvydas.liekis@gmail.com>
2021-01-16 19:12:03 +0200

    added coin json consumer file to backend/entity

--- backend/src/main/java/com/wild/backend/enitity/Coin.java
+++ backend/src/main/java/com/wild/backend/enitity/Coin.java
@@ -1,4 +1,50 @@
 package com.wild.backend.enitity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@ToString
 public class Coin {
+    private String id;
+    private String name;
+    private String symbol;
+    private BigDecimal price;
+
+    public Coin(){
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
 }
