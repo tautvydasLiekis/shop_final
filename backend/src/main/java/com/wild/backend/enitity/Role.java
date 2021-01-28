78644e30c23bb920b06e2f44c1d5fd0fad342c6a
Crystal <tautvydas.liekis@gmail.com>
2021-01-28 19:15:26 +0200

    added user and role entity's

--- backend/src/main/java/com/wild/backend/enitity/Role.java
+++ backend/src/main/java/com/wild/backend/enitity/Role.java
@@ -1,4 +1,27 @@
 package com.wild.backend.enitity;
 
-public class Role {
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Role implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "role_name")
+    private String roleName;
+
+    @Override
+    public String getAuthority() {
+        return "ROLE_" + roleName;
+    }
 }
--- backend/src/main/java/com/wild/backend/enitity/User.java
+++ backend/src/main/java/com/wild/backend/enitity/User.java
@@ -1,4 +1,75 @@
 package com.wild.backend.enitity;
 
-public class User {
+import lombok.*;
+import org.hibernate.annotations.Cascade;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Collection;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    @NotBlank
+    @Size(min = 3, max = 200)
+    private String username;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String lastname;
+
+    @Column(nullable = false)
+    @Size(min = 3)
+    @NotBlank
+    private String password;
+
+    @ManyToMany
+    @Cascade(org.hibernate.annotations.CascadeType.PERSIST)
+    @JoinTable(
+            name="User_Roles",
+            joinColumns = { @JoinColumn(name = "user_id") },
+            inverseJoinColumns = { @JoinColumn(name = "role_id") }
+    )
+    private Set<Role> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
