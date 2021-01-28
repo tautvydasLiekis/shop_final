a302d32399b00347baeacf752df98b2b54b10260
Crystal <tautvydas.liekis@gmail.com>
2021-01-28 19:08:25 +0200

    added gpu obj

--- backend/src/main/java/com/wild/backend/enitity/Gpu.java
+++ backend/src/main/java/com/wild/backend/enitity/Gpu.java
@@ -1,4 +1,25 @@
 package com.wild.backend.enitity;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Data
+@Entity
 public class Gpu {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private BigDecimal msrpPrice;
+
+    @Column(nullable = false)
+    private Double hashRate;
+
+    @Column(nullable = false)
+    private Double powerW;
+
 }
