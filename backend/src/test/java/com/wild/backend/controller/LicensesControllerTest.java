b760608b704574ce91710bf4e62d1e8375aa9fd7
Crystal <tautvydas.liekis@gmail.com>
2021-02-08 17:58:22 +0200

    tests

--- backend/src/test/java/com/wild/backend/controller/LicensesControllerTest.java
+++ backend/src/test/java/com/wild/backend/controller/LicensesControllerTest.java
@@ -1,26 +1,40 @@
-package com.wild.backend.controllers;
+package com.wild.backend.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@WithMockUser
-public class ProductControllerTest {
+class LicensesControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
     @Test
-    void should_return_ok_for_products() throws Exception {
-        mvc.perform(get("/products"))
+    void allLicenses() throws Exception {
+        mvc.perform(get("/licenses/all"))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    void registerFakeKey() throws Exception {
+        mvc.perform(patch("/licenses").param("licenseKey", "1541511"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void getRegisteredKeyStatus() {
+    }
+
+    @Test
+    void getUsersLicenses() {
     }
 }
