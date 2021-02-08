69fa426977d649bbbdce027fa3e8689ddd722412
Crystal <tautvydas.liekis@gmail.com>
2021-02-08 17:09:39 +0200

    fixed react api

--- ui/src/api/licenseApi.js
+++ ui/src/api/licenseApi.js
@@ -1,5 +1,5 @@
 import HTTP from './'
 
-export const allKeys = () => HTTP.get(`/licenses`)
+export const allKeys = () => HTTP.get(`/licenses/all`)
 export const registerKey = (licenseKey) => HTTP.post(`/licenses/${licenseKey}`)
 export const keyStatus = (licenseKey) => HTTP.get(`/licenses/${licenseKey}`)
